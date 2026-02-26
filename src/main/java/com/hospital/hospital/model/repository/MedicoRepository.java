5fa63dcca3bd4203d4573f8881b4cb850a089346
AIDA GUADALUPE GONZALEZ GONZALEZ <aidalu13oct@gmail.com>
2026-02-25 23:39:42 -0600

    Se agregan endpoints para perfil de medico

--- src/main/java/com/hospital/hospital/model/repository/MedicoRepository.java
+++ src/main/java/com/hospital/hospital/model/repository/MedicoRepository.java
@@ -1,10 +1,12 @@
 package com.hospital.hospital.model.repository;
 
+import com.hospital.hospital.model.entity.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.hospital.hospital.model.entity.Medico;
+import java.util.Optional;
 
-public interface MedicoRepository extends JpaRepository<Medico,Integer>{
+public interface MedicoRepository extends JpaRepository<Medico, Integer> {
     boolean existsByCedulaProfesional(String cedulaProfesional);
-    
+    boolean existsByNumEmpleado(String numEmpleado);
+    Optional<Medico> findByUsuarioIdUsuario(Integer idUsuario);
 }
---
+++ src/main/java/com/hospital/hospital/model/repository/UnidadMedicaRepository.java
@@ -0,0 +1,7 @@
+package com.hospital.hospital.model.repository;
+
+import com.hospital.hospital.model.entity.UnidadMedica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UnidadMedicaRepository extends JpaRepository<UnidadMedica, Short> {
+} 
